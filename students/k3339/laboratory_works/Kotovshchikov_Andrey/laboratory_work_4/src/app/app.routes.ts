09aa979daed0d54374b5a04d855fa80dc3c6afcb
Andrey <ykt_andrey@mail.ru>
2024-12-15 17:15:36 +0300

    feat(room-booking): added error messages instead alerts

--- students/k3339/laboratory_works/Kotovshchikov_Andrey/laboratory_work_4/src/app/app.routes.ts
+++ students/k3339/laboratory_works/Kotovshchikov_Andrey/laboratory_work_4/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { AppComponent } from './app.component';
 import { ReportComponent } from './report/report.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { RoomListComponent } from './room-list/room-list.component';
--- students/k3339/laboratory_works/Kotovshchikov_Andrey/laboratory_work_4/src/app/room-booking/room-booking.component.ts
+++ students/k3339/laboratory_works/Kotovshchikov_Andrey/laboratory_work_4/src/app/room-booking/room-booking.component.ts
@@ -1,5 +1,12 @@
 import { NgIf } from '@angular/common';
-import { Component, input, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  computed,
+  effect,
+  input,
+  signal,
+} from '@angular/core';
 import { HotelService } from '../hotel.service';
 import { finalize } from 'rxjs';
 import {
@@ -11,6 +18,7 @@ import {
 } from '@angular/forms';
 import { ICreateBookingRequest } from '../types';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'room-booking-modal',
@@ -34,8 +42,18 @@ export class RoomBookingComponent {
   readonly roomId = input.required<number>();
   readonly isModalOpen = input.required<boolean>();
   readonly isLoading = signal<boolean>(false);
+  readonly errorMessage = signal<string>('');
+  readonly successMessage = signal<string>('');
 
-  constructor(private readonly hotelService: HotelService) {}
+  constructor(private readonly hotelService: HotelService) {
+    effect(
+      () => {
+        this.isModalOpen();
+        this.clearMessages();
+      },
+      { allowSignalWrites: true }
+    );
+  }
 
   createBooking(event: Event) {
     event.preventDefault();
@@ -48,30 +66,37 @@ export class RoomBookingComponent {
       check_out_date: this.form.controls.checkOutDate.value,
     };
 
+    this.clearMessages();
     this.isLoading.set(true);
     this.hotelService
       .createBooking(this.roomId(), data)
       .pipe(finalize(() => this.isLoading.set(false)))
       .subscribe({
-        next: () => alert('Номер успешно забронирован'),
+        next: () => this.successMessage.set('Клиент заселен'),
         error: (err: HttpErrorResponse) => {
           console.log(err);
           if ('detail' in err.error) {
-            alert(err.error['detail']);
+            this.errorMessage.set(err.error['detail']);
           } else {
-            alert('Что то пошло не так, повторите позже');
+            this.errorMessage.set('Что то пошло не так, повторите позже');
           }
         },
       });
   }
 
   vacateRoom() {
+    this.clearMessages();
     this.isLoading.set(true);
     this.hotelService
       .vacateRoom(this.roomId())
       .pipe(finalize(() => this.isLoading.set(false)))
       .subscribe({
-        next: () => alert('Номер успешно освобожден'),
+        next: () => this.successMessage.set('Номер освобожден'),
       });
+  }
+
+  private clearMessages() {
+    this.successMessage.set('');
+    this.errorMessage.set('');
   }
 }
